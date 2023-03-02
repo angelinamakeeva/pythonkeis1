ea9b170023f9c542109e7c5ff5bb2eacc507ed07
angelinamakeeva <125530626+angelinamakeeva@users.noreply.github.com>
2023-03-02 07:14:53 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+'''
+Title 
+Group:
+Makeeva Angelina
+Kareva Alenf
+'''
+
+def triangle(x, y, a, b, angle, color):
+    '''
+    :param x:
+    :param y:
+    :param a:
+    :param b:
+    :param angle:
+    :param color:
+    '''
+
+
+
+if __name__ == "_main_":
+    main()
